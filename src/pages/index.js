93abfadef5e7c392e8e49372cc7c661e384dba66
Schyler Manning <schmann44@gmail.com>
2022-11-08 15:18:39 -0600

    additional updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/layout'
+import Button from '../components/button'
+import * as Styles from '../components/styles.module.css'
 
 const pageStyles = {
   color: "#232129",
@@ -10,9 +13,11 @@ const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
+  fontFamily: "sans-serif",
 }
 const headingAccentStyles = {
   color: "#bf1818",
+  fontFamily: "sans-serif",
 }
 const paragraphStyles = {
   marginBottom: 48,
@@ -56,47 +61,80 @@ const descriptionStyle = {
   lineHeight: 1.25,
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+const quizQuestion = {
+  color: "#E57321",
+  fontWeight: "bold",
+  fontSize: 20,
+  width: "100%",
 }
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+const none = "none";
+var selectedGeo = none;
+const geo = "geoButtons";
+const movieData = require("../json/movies.json");
+var recommendation;
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Christmas Movie Database
+    <Layout>
+      <div id="title">
+        <h1 style={headingStyles}>
+          Christmas Movie Database
+          <br />
+          <span style={headingAccentStyles}>— a site to help you get in the holiday spirit!</span>
+        </h1>
+        <h2 style={headingAccentStyles}>
+          Welcome! Let me help you pick a Christmas movie to get in the holiday spirit! 🎄
+        </h2>
+      </div>
+      <div id="quiz">
+        <div style={quizQuestion}>Pick your location: </div>
         <br />
-        <span style={headingAccentStyles}>— a site to help you get in the holiday spirit!</span>
-      </h1>
-      <p style={paragraphStyles}>
-        WIP: a collection of Christmas movies and filters to help you choose based on your mood 🎄
-      </p>
-      <StaticImage
+        <Button
+          name="US"
+          value="United States"
+          id="geo.US"
+          className={Styles.button}
+          onClick={buttonPressed}
+        />
+        <Button
+          name="abroad"
+          value="Abroad"
+          id="geo.abroad"
+          className={Styles.button}
+          onClick={buttonPressed}
+        />
+      </div>
+      {/* <StaticImage
         alt="Closeup christmas tree with ornaments - source Unsplash"
         src="../images/christmas-tree-unsplash.jpg"
-      />
-    </main>
+        width={200}
+        height={200}
+      /> */}
+    </Layout>
   )
 }
 
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
+
+function toggleGeo(e) {
+  if (selectedGeo === e.target.id) {
+    e.target.className = Styles.button;
+    selectedGeo = none;
+  } else {
+    if (selectedGeo !== none) {
+      document.getElementById(selectedGeo).className = Styles.button;
+    }
+
+    e.target.className = Styles.buttonSelected;
+    selectedGeo = e.target.id;
+  }
+}
+
+function buttonPressed(e) {
+  if (e.target.nam === geo) {
+    toggleGeo(e);
+  }
+}
