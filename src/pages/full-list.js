aae5fd049f2f5e82ced22148ab35ae4182c65ea0
Schyler Manning <schmann44@gmail.com>
2022-11-21 21:25:43 -0600

    add initial christmas db content to site

--- src/pages/full-list.js
+++ src/pages/full-list.js
@@ -2,17 +2,17 @@ import React from 'react'
 import JSONData from '../../json/movies.json'
 import Layout from '../components/layout'
 
-const fullList = () => {
-    return (
-        <Layout>
-            <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-                <h1>{JSONData.title}</h1>
-                <ul>
-                {JSONData.content.map((data, index) => (
-                    <li key={`content_title_${index}`}>{data.title}</li>
-                ))}
-                </ul>
-            </div>
-        </Layout>
-)}
-export default fullList
+// const fullList = () => {
+//     return (
+//         <Layout>
+//             <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+//                 <h1>{JSONData.title}</h1>
+//                 <ul>
+//                 {JSONData.content.map((data, index) => (
+//                     <li key={`content_title_${index}`}>{data.title}</li>
+//                 ))}
+//                 </ul>
+//             </div>
+//         </Layout>
+// )}
+// export default fullList
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,113 +4,103 @@ import Layout from '../components/layout'
 import Button from '../components/button'
 import * as Styles from '../components/styles.module.css'
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-  fontFamily: "sans-serif",
-}
-const headingAccentStyles = {
-  color: "#bf1818",
-  fontFamily: "sans-serif",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const quizQuestion = {
-  color: "#E57321",
-  fontWeight: "bold",
-  fontSize: 20,
-  width: "100%",
-}
-
 const none = "none";
 var selectedGeo = none;
+var selectedMood = none;
+var selectedSanta = none;
 const geo = "geoButtons";
-const movieData = require("../json/movies.json");
+const mood = "moodButtons";
+const santa = "santaButtons";
+const movieData = require("../../json/movies.json");
 var recommendation;
 
 const IndexPage = () => {
   return (
     <Layout>
       <div id="title">
-        <h1 style={headingStyles}>
+        <h1 className={Styles.heading}>
           Christmas Movie Database
           <br />
-          <span style={headingAccentStyles}>— a site to help you get in the holiday spirit!</span>
+          <span className={Styles.headingAccent}>— a site to help you get in the holiday spirit!</span>
         </h1>
-        <h2 style={headingAccentStyles}>
+        <h2 className={Styles.headingAccent}>
           Welcome! Let me help you pick a Christmas movie to get in the holiday spirit! 🎄
         </h2>
       </div>
       <div id="quiz">
-        <div style={quizQuestion}>Pick your location: </div>
+        <div className={Styles.quizQuestion}>Pick a location: </div>
         <br />
-        <Button
-          name="US"
-          value="United States"
-          id="geo.US"
-          className={Styles.button}
-          onClick={buttonPressed}
-        />
-        <Button
-          name="abroad"
-          value="Abroad"
-          id="geo.abroad"
-          className={Styles.button}
-          onClick={buttonPressed}
-        />
+        <div>
+          <Button
+            name="geoButtons"
+            value="United States"
+            id="geo.US"
+            className={Styles.button}
+            onClick={buttonPressed}
+          />
+          <Button
+            name="geoButtons"
+            value="Abroad"
+            id="geo.abroad"
+            className={Styles.button}
+            onClick={buttonPressed}
+          />
+        </div>
+        <br />
+        <div className={Styles.quizQuestion}>Pick a mood: </div>
+        <br />
+        <div>
+          <Button
+            name="moodButtons"
+            value="Funny"
+            id="mood.funny"
+            className={Styles.button}
+            onClick={buttonPressed}
+          />
+        </div>
+        <div>
+          <Button
+            name="moodButtons"
+            value="Emotional"
+            id="mood.emotional"
+            className={Styles.button}
+            onClick={buttonPressed}
+          />
+        </div>
+        <br />
+        <div className={Styles.quizQuestion}>Santa or no Santa? </div>
+        <br />
+        <div>
+          <Button
+            name="santaButtons"
+            value="Santa"
+            id="santa.yes"
+            className={Styles.button}
+            onClick={buttonPressed}
+          />
+        </div>
+        <div>
+          <Button
+            name="santaButtons"
+            value="No Santa"
+            id="santa.no"
+            className={Styles.button}
+            onClick={buttonPressed}
+          />
+        </div>
       </div>
-      {/* <StaticImage
-        alt="Closeup christmas tree with ornaments - source Unsplash"
-        src="../images/christmas-tree-unsplash.jpg"
-        width={200}
-        height={200}
-      /> */}
+      <div id="recommendationHeader" className={Styles.recommendationHeader}>
+        <br />
+        You should check out:
+        <br />
+      </div>
+      <div className={Styles.recommendationBody} id="recommendationBody">
+        <br />
+        <br />
+        <div id="movieTitle" className={Styles.movieTitle}></div>
+        <br />
+        <div id="movieDescription" className={Styles.movieDescription}></div>
+      </div>
     </Layout>
   )
 }
@@ -133,8 +123,78 @@ function toggleGeo(e) {
   }
 }
 
-function buttonPressed(e) {
-  if (e.target.nam === geo) {
-    toggleGeo(e);
+function toggleMood(e) {
+  if (selectedMood === e.target.id) {
+    e.target.className = Styles.button;
+    selectedMood = none;
+  } else {
+    if (selectedMood !== none) {
+      document.getElementById(selectedMood).className = Styles.button;
+    }
+
+    e.target.className = Styles.buttonSelected;
+    selectedMood = e.target.id;
   }
+}
+
+function toggleSanta(e) {
+  if (selectedSanta === e.target.id) {
+    e.target.className = Styles.button;
+    selectedSanta = none;
+  } else {
+    if (selectedSanta !== none) {
+      document.getElementById(selectedSanta).className = Styles.button;
+    }
+
+    e.target.className = Styles.buttonSelected;
+    selectedSanta = e.target.id;
+  }
+}
+
+function buttonPressed(e) {
+  // e.target.className = Styles.buttonSelected;
+  if (e.target.name === geo) {
+    toggleGeo(e);
+  } else if (e.target.name === mood) {
+    toggleMood(e);
+  } else if (e.target.name === santa) {
+    toggleSanta(e);
+  }
+  if (selectionsComplete()) {
+    generateRecommendation();
+  }
+}
+
+function selectionsComplete() {
+  if ( selectedGeo !== none && selectedMood !== none && selectedSanta !== none) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function generateRecommendation() {
+  var selectedTags = [selectedGeo, selectedMood, selectedSanta];
+
+  var filteredMovies = movieData.filter((movie) => 
+    selectedTags.every((t) => movie.tags.includes(t))
+  );
+
+  if (filteredMovies.length === 0) {
+    return;
+  }
+
+  recommendation = filteredMovies[Math.floor(Math.random() * filteredMovies.length)];
+  var titleDiv = document.getElementById("movieTitle");
+  var descriptionDiv = document.getElementById("movieDescription");
+  var recHeaderDiv = document.getElementById("recommendationHeader");
+  var recBodyDiv = document.getElementById("recommendationBody");
+
+  recHeaderDiv.style.display = "block";
+  recBodyDiv.style.display = "block";
+  titleDiv.innerHTML = recommendation.title;
+  descriptionDiv.innerHTML = recommendation.description;
+  titleDiv.style.display = "block";
+  descriptionDiv.style.display = "block";
+
 }
